47de975ca8e4623b3d24afb9b36b2c620b86d2bf
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2017-11-26 14:17:40 +0300

    create user roster

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # xmpp-roster-create
-adding users to roster group by reading  from OpenLDAP
+
+Kullanıcı roster grubuna eklenmek istenen ahenk listesi Ldap'tan alınmaktadır. 
+KOMUT= **./ejabberdctl add_rosteritem lider_console im.mys.tuncay.colak  ahenk-pc im.tuncay.colak '' '' both**
+Yukarıdaki komuttan belirtilen lider_console roster grubu oluşturulmak iistenen kullanıcı ve im.tuncay.colak ise xmpp servis adıdır. Bu değerler betik çalıştırılmadan önce parametre olarak girilir.
+Betik içinde yer alan 
+  **  
+  	hostname="192.168.56.102"
+  	search_base = "dc=tuncay,dc=colak"
+    base_dn = "cn=admin,"+str(search_base)
+    pwd = "1"
+    ldap_obj = ldap.open(hostname)
+    **
+    
+ldap bağlantısı için gerekli 
+hostname, searc_base ve pwd değerleri düzenlenmelidir.
+Bu alanlar girildikten sora ilgli betik xmpp sunucusunda örneğin /opt/ejabberd-16.06/bin dizinine koplayalnmalıdır. 
+
+**python python add-roster-groups.py "kullanıcı adı" "xmpp servis adı"**
+
+komutu ile çalıştırtılır.
---
+++ add-roster-groups.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
+# xmpp de roster grubu oluşturma 
+
+import sys
+import os
+import ldap
+import ldap.modlist as modlist
+
+"""
+KOMUT= ./ejabberdctl add_rosteritem lider_console im.mys.pardus.org.tr  ahenk-pc im.mys.pardus.org.tr '' '' both
+bu betik ve ahenk_list dosyası /opt/ejabberd-16.06/bin dizin altına kopyalanmalıdır. İlgili betik ldap tan ahenkleri okuyarak kullanıcı roster 
+grubuna eklemektedir.Ldap bilgileri girildikten sonra python add-roster-groups.py "kullanıcı adı" "xmpp servis adı" komutu ile çalıştırılmalıdır.
+
+"""
+def add_roster_1(cmd_1):
+    os.system(cmd_1)
+    print "run command_1 is okk"
+
+def add_roster_2(cmd_2):
+    os.system(cmd_2)
+    print "run command_2 is okk"
+    print "---------------------------------------------------------------------------------------------------------------------"
+
+def get_agent_uid(username,xmpp_servis_name):
+
+    hostname="192.168.56.102"
+    search_base = "dc=tuncay,dc=colak"
+    base_dn = "cn=admin,"+str(search_base)
+    pwd = "1"
+    ldap_obj = ldap.open(hostname)
+    ldap_obj.simple_bind_s(base_dn,pwd)
+    print "ldap'a bağlantı kuruldu......"
+    search_scope = ldap.SCOPE_SUBTREE
+    try:
+        searchAttribute = ["uid"]
+        search_filter = "(objectClass=pardusDevice)"
+        ldap_result = ldap_obj.search_s(search_base, search_scope, search_filter, searchAttribute)
+        ldap_list = []
+        for result in ldap_result:
+            #print (result[1]["uid"][0])
+            agent_uid = result[1]["uid"][0]
+            print"ahenk uid degeri--->> "+str(agent_uid)
+            create_roster(agent_uid,username,xmpp_servis_name)
+            print "----->> add agent to roster <<<-----"
+    except Exception as e:
+        print "ahenk bulunamadı. "+str(e)    
+    ldap_obj.unbind_s()   
+
+def create_roster(agent_uid,username,xmpp_servis_name):
+
+    print"agent_uid: -->> "+str(agent_uid)
+    cmd_1 = "./ejabberdctl add_rosteritem "+str(username)+" "+str(xmpp_servis_name)+" "+str(agent_uid)+" "+str(xmpp_servis_name)+" '' '' both"
+    print "command_1: "+str(cmd_1)
+    add_roster_1(cmd_1)
+    cmd_2 = "./ejabberdctl add_rosteritem "+str(agent_uid)+" "+str(xmpp_servis_name)+" "+str(username)+" "+str(xmpp_servis_name)+" '' '' both"
+    print "command_2: "+str(cmd_2)
+    add_roster_2(cmd_2)
+
+if __name__ == '__main__':
+    
+    username = sys.argv[1]
+    xmpp_servis_name = sys.argv[2]
+    get_agent_uid(username,xmpp_servis_name)
