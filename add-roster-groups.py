d9542b4e7c67201c85b1456b1098b82bb23480d1
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2017-12-12 00:02:38 +0300

    delete roster group

--- add-roster-groups.py
+++ add-roster-groups.py
@@ -8,52 +8,81 @@ import os
 import ldap
 import ldap.modlist as modlist
 
-def add_roster_1(cmd_1):
-    os.system(cmd_1)
-    print "run command_1 is okk"
 
-def add_roster_2(cmd_2):
-    os.system(cmd_2)
-    print "run command_2 is okk"
-    print "---------------------------------------------------------------------------------------------------------------------"
+class RosterItem(object):
+    """docstring for RosterItem"""
+    def __init__(self):
+        super(RosterItem, self).__init__()
+        self.username = raw_input("Roster grubu oluşturmak istediğiniz kullanıcı adını giriniz: ")
+        self.xmpp_servis_name = raw_input("xmpp servis adını giriniz [eg:im.liderahenk.org]: ")
+              
 
-def get_agent_uid(username,xmpp_servis_name):
+    def get_agent_uid(self):
 
-    hostname="192.168.56.102"
-    search_base = "dc=tuncay,dc=colak"
-    base_dn = "cn=admin,"+str(search_base)
-    pwd = "1"
-    ldap_obj = ldap.open(hostname)
-    ldap_obj.simple_bind_s(base_dn,pwd)
-    print "ldap'a bağlantı kuruldu......"
-    search_scope = ldap.SCOPE_SUBTREE
-    try:
-        searchAttribute = ["uid"]
-        search_filter = "(objectClass=pardusDevice)"
-        ldap_result = ldap_obj.search_s(search_base, search_scope, search_filter, searchAttribute)
-        ldap_list = []
-        for result in ldap_result:
-            #print (result[1]["uid"][0])
-            agent_uid = result[1]["uid"][0]
-            print"ahenk uid degeri--->> "+str(agent_uid)
-            create_roster(agent_uid,username,xmpp_servis_name)
-            print "----->> add agent to roster <<<-----"
-    except Exception as e:
-        print "ahenk bulunamadı. "+str(e)    
-    ldap_obj.unbind_s()   
+        hostname="192.168.56.111"
+        search_base = "dc=liderahenk,dc=org"
+        base_dn = "cn=admin,"+str(search_base)
+        pwd = "1"
+        ldap_obj = ldap.open(hostname)
+        ldap_obj.simple_bind_s(base_dn,pwd)
+        print "ldap'a bağlantı kuruldu......"
+        search_scope = ldap.SCOPE_SUBTREE
+        try:
+            searchAttribute = ["uid"]
+            search_filter = "(objectClass=pardusDevice)"
+            ldap_result = ldap_obj.search_s(search_base, search_scope, search_filter, searchAttribute)
+            agent_uid_list = []
+            for result in ldap_result:
+                #print (result[1]["uid"][0])
+                agent_uid = result[1]["uid"][0]
+                #print"ahenk uid degeri--->> "+str(agent_uid)
+                agent_uid_list.append(agent_uid)
+                #print agent_uid_list
+        except Exception as e:
+            print "ahenk bulunamadı. "+str(e)
+        return agent_uid_list
+        ldap_obj.unbind_s() 
+        #close ldap server
 
-def create_roster(agent_uid,username,xmpp_servis_name):
+    # create roster group
+    def create_roster(self):
+        agents = self.get_agent_uid()
 
-    print"agent_uid: -->> "+str(agent_uid)
-    cmd_1 = "./ejabberdctl add_rosteritem "+str(username)+" "+str(xmpp_servis_name)+" "+str(agent_uid)+" "+str(xmpp_servis_name)+" '' '' both"
-    print "command_1: "+str(cmd_1)
-    add_roster_1(cmd_1)
-    cmd_2 = "./ejabberdctl add_rosteritem "+str(agent_uid)+" "+str(xmpp_servis_name)+" "+str(username)+" "+str(xmpp_servis_name)+" '' '' both"
-    print "command_2: "+str(cmd_2)
-    add_roster_2(cmd_2)
+        for agent_uid in agents:         
+            print"agent_uid: -->> "+str(agent_uid)
+            cmd_1 = "./ejabberdctl add_rosteritem "+str(self.username)+" "+str(self.xmpp_servis_name)+" "+str(agent_uid)+" "+str(self.xmpp_servis_name)+" '' '' both"
+            print "command_1: "+str(cmd_1)
+            os.system(cmd_1)
+
+            cmd_2 = "./ejabberdctl add_rosteritem "+str(agent_uid)+" "+str(self.xmpp_servis_name)+" "+str(self.username)+" "+str(self.xmpp_servis_name)+" '' '' both"
+            print "command_2: "+str(cmd_2)
+            os.system(cmd_2)
+            print "----->>  add agent to roster <<<-----"
+            print "---------------------------------------------------------------------------------------------------------------------"
+
+    # delete roster group
+    def del_roster(self):
+        agents = self.get_agent_uid()
+
+        for agent_uid in agents:         
+            print"agent_uid: -->> "+str(agent_uid)
+            cmd_1 = "./ejabberdctl del_rosteritem "+str(self.username)+" "+str(self.xmpp_servis_name)+" "+str(agent_uid)+" "+str(self.xmpp_servis_name)+" '' '' both"
+            print "command_1: "+str(cmd_1)
+            os.system(cmd_1)
+
+            cmd_2 = "./ejabberdctl del_rosteritem "+str(agent_uid)+" "+str(self.xmpp_servis_name)+" "+str(self.username)+" "+str(self.xmpp_servis_name)+" '' '' both"
+            print "command_2: "+str(cmd_2)
+            os.system(cmd_2)
+            print "----->>  delete agent to roster <<<-----"
+            print "---------------------------------------------------------------------------------------------------------------------"
+
 
 if __name__ == '__main__':
-    
-    username = sys.argv[1]
-    xmpp_servis_name = sys.argv[2]
-    get_agent_uid(username,xmpp_servis_name)
+        
+    if sys.argv[1] == 'add':
+        app = RosterItem()
+        app.create_roster()
+
+    elif sys.argv[1] == 'del':
+        app = RosterItem()
+        app.del_roster()
